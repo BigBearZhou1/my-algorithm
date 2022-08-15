9ef148d1783571d2cbdea8a9977275b965c16a46
BigBearZhou1 <zhouwenzong@126.com>
2022-08-15 22:43:26 +0800

    折纸

--- src/main/java/algorithm/tree/PaperFolding.java
+++ src/main/java/algorithm/tree/PaperFolding.java
@@ -1,4 +1,23 @@
 package algorithm.tree;
 
+/**
+ * 打印对折折痕
+ */
 public class PaperFolding {
+    public static void printAllFolds(int N) {
+        printProcess(1, N, true);
+    }
+
+    private static void printProcess(int i, int n, boolean b) {
+        if (i > n) {
+            return;
+        }
+        printProcess(i + 1, n, true);
+        System.out.println(b ? "down" : "up");
+        printProcess(i + 1, n, false);
+    }
+
+    public static void main(String[] args) {
+        printAllFolds(2);
+    }
 }
