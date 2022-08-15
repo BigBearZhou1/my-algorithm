a28da7785d65897ce0d0323f5eb4fa9161f542a9
BigBearZhou1 <zhouwenzong@126.com>
2022-08-15 22:49:51 +0800

    二叉树的最长距离

--- src/main/java/algorithm/tree/MaxDistance.java
+++ src/main/java/algorithm/tree/MaxDistance.java
@@ -1,4 +1,37 @@
 package algorithm.tree;
 
+import algorithm.dp.Node;
+import algorithm.entity.TreeNode;
+import sun.applet.AppletResourceLoader;
+
+/**
+ * 二叉树的最长距离
+ */
 public class MaxDistance {
+    class Info {
+        int height;
+        int maxDistance;
+
+        public Info(int height, int maxDistance) {
+            this.height = height;
+            this.maxDistance = maxDistance;
+        }
+    }
+
+    public int findMaxDistance(TreeNode node) {
+        return process(node).maxDistance;
+    }
+
+    private Info process(TreeNode node) {
+        if (node == null) {
+            return new Info(0, 0);
+        }
+        Info leftInfo = process(node.left);
+        Info rightInfo = process(node.right);
+
+        int height = Math.max(leftInfo.height, rightInfo.height) + 1;
+        int maxDistance = Math.max(Math.max(leftInfo.maxDistance, rightInfo.maxDistance),
+                leftInfo.height + rightInfo.height + 1);
+        return new Info(height, maxDistance);
+    }
 }
